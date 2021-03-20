c5e33330e9eb9f80c521b0075587f293cb38b7ef
amanchawla007 <amanchawla25july@github.com>
2021-03-20 17:51:58 +0530

    update pages

--- MH/index.js
+++ MH/index.js
@@ -8,7 +8,7 @@ const User = require('./models/user');
 
 const bcrypt = require('bcrypt');
 const session = require("express-session");
-const db = "MH_4";
+const db = "MH_100";
 mongoose.connect('mongodb://localhost:27017/'+db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log("MONGO CONNECTION OPEN") }).catch(err => {
     console.log("THERE IS A PROBLEM");
     console.log(err)
@@ -74,7 +74,7 @@ app.get("/portal/:id",requireLogin,(req,res)=>{
             if(user.Position=="Admin"){
                 res.redirect("/admin1/"+_id);
             }
-            if(user.Position=="chari"){
+            if(user.Position=="chari"||user.Position=="D"||user.Position=="C"){
             res.render("admin",{user:user,allUser:allUser});}
             if(user.Position=="vol"){
                 res.send("Vol");
@@ -84,12 +84,32 @@ app.get("/portal/:id",requireLogin,(req,res)=>{
 });
 app.get("/admin1/:id",requireLogin,(req,res)=>{
     User.find({}).then(users=>{
-        console.log(users);
-        res.render("admin1",{users:users});
+        
+        res.render("admin1",{users:users,id:req.params.id});
     });
 });
 app.get("/event",(req,res)=>{
     res.render("event");
+});
+app.post("/cp/:d/:_id/:id",(req,res)=>{
+    if(req.params.d=="accept"){
+        User.findByIdAndUpdate(req.params._id,{"Position":"C"},(err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.redirect("/admin1/"+req.params.id);
+            }
+        });
+    }
+    if(req.params.d=="D"){
+        User.findByIdAndUpdate(req.params._id,{"Position":"D"},(err,result)=>{
+            if(err){
+                res.send(err);
+            }else{
+                res.redirect("/admin1/"+req.params.id);
+            }
+        });
+    }
 });
 // REGISTER AS ADMIN, VOLENTEER and DONATOR
 app.get("/register",(req,res)=>{
@@ -147,7 +167,7 @@ app.post("/register1",async(req,res)=>{
             if(countUser==0){
                 //If the userCount is 0 make the position as Admin
                 const user=new User({
-                    Name,Email,Password:hash,Address,text,age,sex,reason,Position:"chari",Coor
+                    Name,Email,Password:hash,Address,text,age,sex,reason,Position:"Admin",Coor
                 });
                 user.save();
             }else{
